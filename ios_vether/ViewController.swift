526891c22a3d56216979db47993d1de9d1ae9949
savoptik <sav70792@gmail.com>
2019-07-11 19:20:30 +0300

    Initial Commit

---
+++ ios_vether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_vether
+//
+//  Created by Артём Семёнов on 11.07.2019.
+//  Copyright © 2019 Артём Семёнов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
