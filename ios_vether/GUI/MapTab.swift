1eb1ebf6694c16d17f87e194e8851a258cabec30
savoptik <sav70792@gmail.com>
2019-07-12 13:58:55 +0300

    Налажено определение геопозиции пользователя

--- ios_vether/GUI/MapTab.swift
+++ ios_vether/GUI/MapTab.swift
@@ -20,37 +20,35 @@ class MapTab: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
         self.view.addSubview(self.mapView)
         let constr = [self.view.safeAreaLayoutGuide.topAnchor.constraint(equalToSystemSpacingBelow: self.mapView.topAnchor, multiplier: 1), self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalToSystemSpacingBelow: self.mapView.bottomAnchor, multiplier: 1), self.view.safeAreaLayoutGuide.leftAnchor.constraint(equalToSystemSpacingAfter: self.mapView.leftAnchor, multiplier: 1), self.view.safeAreaLayoutGuide.rightAnchor.constraint(equalToSystemSpacingAfter: self.mapView.rightAnchor, multiplier: 1)] // задаём размеры
         NSLayoutConstraint.activate(constr) // расчитываем констрейны
+        print("test 1")
         if  CLLocationManager.locationServicesEnabled() {
+            print("test 2")
             self.locationManager = CLLocationManager()
             self.locationManager.delegate = self
             self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
             self.locationManager.requestAlwaysAuthorization()
             self.locationManager.requestWhenInUseAuthorization()
             self.locationManager.startUpdatingHeading()
+            print("test 3")
         }
         self.mapView.delegate = self
         self.mapView.mapType = .standard
         self.mapView.isZoomEnabled = true
         self.mapView.isScrollEnabled = true
-        if let coor = mapView.userLocation.location?.coordinate{
+        print("\(self.mapView.userLocation.isUpdating)")
+        if let coor = self.locationManager.location?.coordinate {
+            print("test 4")
             mapView.setCenter(coor, animated: true)
+                let geocoder = CLGeocoder()
+                // Look up the location and pass it to the completion handler
+                geocoder.reverseGeocodeLocation(self.locationManager.location!,
+                                                completionHandler: { (placemarks, error) in
+                                                    if error == nil {
+                                                        let firstLocation = placemarks?[0]
+                                                        print("\(firstLocation)")
+                                                    }
+                })
     }
     }
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        
-        self.mapView.mapType = MKMapType.standard
-        
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: locValue, span: span)
-        self.mapView.setRegion(region, animated: true)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = locValue
-        annotation.title = "Javed Multani"
-        annotation.subtitle = "current location"
-        self.mapView.addAnnotation(annotation)
-        
-        //centerMap(locValue)
-    }
 }
